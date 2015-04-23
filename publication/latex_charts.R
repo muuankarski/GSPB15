24af8d28c8e943070e5aa50f7cf538f2d8eecc78
Markus Kainu <markus.kainu@fao.org>
2015-04-23 19:37:28 +0200

    Tables are now in publication/Tables folder and other minow tweaks

--- publication/latex_charts.R
+++ publication/latex_charts.R
@@ -12,51 +12,34 @@ chart_spread <- function(title="title",
   if (exists("footnoteRight")) footnoteRightText <- paste0("\\footnotesize{",footnoteRight,"}  \n")
   if (!exists("footnoteRight")) footnoteRightText <- "\\vspace{-7pt} \n"
   
+  # cat(paste0("\\begin{ChartPage}{ \\Large{",title,"} } \n")) # This fixes the font size but messes the tOC font size
   cat(paste0("\\begin{ChartPage}{ ",title," } \n"))
   
   cat(paste0("\\LeftText{\\IfFileExists{./Text/",LeftTextCode,".tex}{\\input{./Text/",LeftTextCode,".tex}}{\\lipsum[2]}} \n"))
 
 
-  cat(paste0("\\RightText{\\IfFileExists{./Plots/",RightTextCode,".tex}  
+  cat(paste0("\\RightText{\\IfFileExists{./Tables/",RightTextCode,".tex}  
 	               {\\begin{table} \n
 	               \\input{./Captions/Caption_",RightTextCode,".tex} \n
-	               \\input{./Plots/",RightTextCode,".tex} \n
+	               \\input{./Tables/",RightTextCode,".tex} \n
 	               \\end{table}}} \n \n"))
-  
-#   cat(paste0("\\RightText{\\IfFileExists{./Plots/MT.P1.IAF.1.2.tex} \n 
-# 	               {\\begin{table} \n
-# 	               
-#                   \\scriptsize \n
-#                   \\begin{center} \n
-#                   \\begin{tabular}{lrr} \n
-#                   \\toprule \n
-#                     & 1990-92 & 2012-14\\ \n
-#                   \\midrule \n
-#                   World & 18.7 & 11.3\\ \n
-#                   Oceania & 15.7 & 14\\ \n
-#                   \\toprule \n
-#                   \\end{tabular} \n
-#                   \\end{center} \n
-# 	               
-# 	               \\end{table}}} \n \n"))
-  
-  
+   
 	cat(paste0("\\LeftChart{\\begin{chart} \n 
 	               \\input{./Captions/Caption_",LeftChartCode,".tex} \n",
-	               "\\IfFileExists{./Plots/",LeftChartCode,".pdf}{\\includegraphics[width = 4cm, height = 8cm]{{./Plots/",LeftChartCode,"}.pdf}}{} \n",
+	               "\\IfFileExists{./Plots/",LeftChartCode,".pdf}{\\includegraphics[width = 4.5cm, height = 6.5cm]{{./Plots/",LeftChartCode,"}.pdf}}{} \n",
 	               "\\end{chart}} \n"))
 	
 	cat(paste0("\\RightChart{\\begin{chart} \n 
 	               \\input{./Captions/Caption_",RightChartCode,".tex} \n",
 	               "\\vspace{-7pt} \n",
-	               "\\IfFileExists{./Plots/",RightChartCode,".pdf}{\\includegraphics[width = 4cm, height = 8cm]{{./Plots/",RightChartCode,"}.pdf}}{} \n",
+	               "\\IfFileExists{./Plots/",RightChartCode,".pdf}{\\includegraphics[width = 4.5cm, height = 6.5cm]{{./Plots/",RightChartCode,"}.pdf}}{} \n",
 	               "\\end{chart}} \n"))
 
 	
   cat(paste0("\\BottomChart{\\begin{chart} \n 
 	               \\input{./Captions/Caption_",BottomChartCode,".tex} \n",
 	           "\\vspace{-7pt} \n",
-	           "\\IfFileExists{./Plots/",BottomChartCode,".pdf}{\\includegraphics[width = 8cm, height = 3cm]{{./Plots/",BottomChartCode,"}.pdf}}{} \n",
+	           "\\IfFileExists{./Plots/",BottomChartCode,".pdf}{\\includegraphics[width = 8cm, height = 3.5cm]{{./Plots/",BottomChartCode,"}.pdf}}{} \n",
 	           "\\end{chart}} \n"))
 	
 	cat("\\end{ChartPage}")
