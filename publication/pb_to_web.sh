d5b2cfc7c724a29efbe386695f6e71765286348a
Markus Kainu <markus.kainu@fao.org>
2015-05-17 18:29:32 +0200

    fixed directory issue..

--- publication/pb_to_web.sh
+++ publication/pb_to_web.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-Rscript -e "library(knitr); knit('~/btsync/fao_sync/pocketbooks/GSPB15/publication/GSPB15.Rnw')"
-pdflatex ~/btsync/fao_sync/pocketbooks/GSPB15/publication/GSPB15.tex
+cd ~/btsync/fao_sync/pocketbooks/GSPB15/publication/
+Rscript -e "library(knitr); knit('GSPB15.Rnw')"
+pdflatex GSPB15.tex
 
 # upload the output pdf to kapsi
 scp GSPB15.pdf muuankarski@kapsi.fi:public_html/fao/GSPB15
