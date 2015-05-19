c409bde8d3726cc144d23622c7aa854ae6d1661b
Markus Kainu Test <markuskainu@gmail.com>
2015-05-19 11:37:14 +0200

    a script for installing dependencies

---
+++ required_packages.R
@@ -0,0 +1,16 @@
+PACKAGES <- c("knitr","Rcpp","ggplot2","devtools","tidyr","dplyr","knitr","reshape2","data.table",
+              "rgdal","zoo","plyr","scales","sp","maptools")
+#  Install packages
+inst <- match(PACKAGES, .packages(all=TRUE))
+need <- which(is.na(inst))
+if (length(need) > 0) install.packages(PACKAGES[need])
+# Load packages
+lapply(PACKAGES, require, character.only=T)
+
+if (!"FAOSTAT" %in% installed.packages()) {
+  install_github(username = "mkao006", repo = "FAOSTATpackage",
+                 ref = "master", subdir = "FAOSTAT")
+}
+if (!"gisfao" %in% installed.packages()) {
+  devtools::install_github('UNFAOstatistics/gisfao')
+}
